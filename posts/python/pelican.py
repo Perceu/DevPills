daaf28568cd9fef920bf7b70b965cdc08bc9c205
Perceu Bertoletti <perceubertoletti@gmail.com>
2023-05-27 16:12:14 -0300

    Adicionado post de iniciado com pelican

---
+++ posts/python/pelican.py
@@ -0,0 +1,24 @@
+"""
+Criando um blog estático com Pelican
+
+$ python -m venv .venv
+$ pip install "pelican[markdown]"
+$ pelican-quickstart
+
+responda as perguntas e escreva o arquivo
+
+.content/hello.md:
+"""
+
+Title: Hello World
+Date: 2023-05-27 15:30
+Category: World
+
+Bem vindo ao Pelican!
+
+"""
+execute os comandos: 
+
+$ pelican content
+$ pelican --listen
+"""
